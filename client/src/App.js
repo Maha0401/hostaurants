f2d418deb77e77468675e1ac25207755e0df3a6d
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-11-28 11:37:55 -0500

    implemented frontend sigin for single user

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import LogIn from './pages/LogIn/LogIn';
+import Dashboard from './pages/Dashboard/Dashboard';
+import SignUp from './pages/SignUp/SignUp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/login" component={LogIn} />
+                </Switch>
+            </div>
+        </BrowserRouter>
   );
 }
 
