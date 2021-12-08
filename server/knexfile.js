dacfbcbb1b113b8174f808683a672ec03a35e231
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-12-08 09:55:38 -0500

    Moved mysql username password to .evv file

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,13 +1,14 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'rootroot',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PSWD,
       database: 'hostaurants',
       charset: 'utf8',
     }
