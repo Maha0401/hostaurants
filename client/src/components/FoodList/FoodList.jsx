e7914388e7c28b1d6541bb52d4a631856d4047be
Mahalakshmi Jegadeesan <mahainfinity@gmail.com>
2021-12-08 13:28:36 -0500

    Fixed the ui issues in foodlists

--- client/src/components/FoodList/FoodList.jsx
+++ client/src/components/FoodList/FoodList.jsx
@@ -11,8 +11,8 @@ class FoodList extends React.Component {
             {foods.map(food => {
                 return(
                     <div onClick={() => this.props.foodClickHandle(food.id,food.chefId)} className='foodlist__food' key = {food.id}>
-                        <h2 className='foodlist__header'>{food.name}</h2>
                         <img className='foodlist__image' src={`http://localhost:8080/${food.pic}`} alt={`${food.name} poster`}></img>
+                        <h2 className='foodlist__header'>{food.name}</h2>
                     </div>
                     )}
             )}        
